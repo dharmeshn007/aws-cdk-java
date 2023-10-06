861edeb6384ba70592593bc7f10ed07baf505971
dharmeshn007 <dharmesh37nadola@gmail.com>
2023-10-06 21:50:28 +0530

    java sample

---
+++ src/main/java/com/myorg/CdkWorkshopStack.java
@@ -0,0 +1,34 @@
+package com.myorg;
+
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
+
+
+public class CdkWorkshopStack extends Stack {
+    public CdkWorkshopStack(final Construct parent, final String id) {
+        this(parent, id, null);
+    }
+
+    public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
+        super(parent, id, props);
+
+         // Defines a new lambda resource
+         final Function hello = Function.Builder.create(this, "HelloHandler")
+         .runtime(Runtime.NODEJS_18_X)    // execution environment
+         .code(Code.fromAsset("lambda"))  // code loaded from the "lambda" directory
+         .handler("hello.handler")        // file is "hello", function is "handler"
+         .build();
+
+         // Defines an API Gateway REST API resource backed by our "hello" function
+        LambdaRestApi.Builder.create(this, "Endpoint")
+        .handler(hello)
+        .build();
+
+        
+    }
+}

58b22163660dbe955d70a3bfdc9d09872ac218a8
dharmeshn007 <dharmesh37nadola@gmail.com>
2023-10-08 00:21:47 +0530

    build

--- src/main/java/com/myorg/CdkWorkshopStack.java
+++ src/main/java/com/myorg/CdkWorkshopStack.java
@@ -8,7 +8,6 @@ import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.apigateway.LambdaRestApi;
 
-
 public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id) {
         this(parent, id, null);
@@ -17,17 +16,30 @@ public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
 
-         // Defines a new lambda resource
-         final Function hello = Function.Builder.create(this, "HelloHandler")
-         .runtime(Runtime.NODEJS_18_X)    // execution environment
-         .code(Code.fromAsset("lambda"))  // code loaded from the "lambda" directory
-         .handler("hello.handler")        // file is "hello", function is "handler"
+        //  // Defines a new lambda resource
+        //  final Function hello = Function.Builder.create(this, "HelloHandler")
+        // //  .runtime(Runtime.NODEJS_18_X)    // execution environment
+        //  .runtime(Runtime.JAVA_11)    // execution environment
+        //  .code(Code.fromAsset("HelloWorldFunction"))  // code loaded from the "lambda" directory
+        //  .handler("App.handler")        // file is "hello", function is "handler"
+        //  .build();
+
+        //  // Defines an API Gateway REST API resource backed by our "hello" function
+        // LambdaRestApi.Builder.create(this, "Endpoint")
+        // .handler(hello)
+        // .build();
+
+         // Define Lambda function
+         Function lambdaFunction = Function.Builder.create(this, "SampleLambdaFunction")
+         .runtime(Runtime.JAVA_11)
+         .code(Code.fromAsset("HelloWorldFunction"))
+         .handler("com.example.YourHandler::handleRequest")
          .build();
 
-         // Defines an API Gateway REST API resource backed by our "hello" function
-        LambdaRestApi.Builder.create(this, "Endpoint")
-        .handler(hello)
-        .build();
+ // Create API Gateway and associate with Lambda function
+        LambdaRestApi.Builder.create(this, "SampleApi")
+         .handler(lambdaFunction)
+         .build();
 
         
     }

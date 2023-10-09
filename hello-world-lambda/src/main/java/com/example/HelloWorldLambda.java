8ccb193b76996e4c436610e5063679bcf6ace35c
dharmeshn007 <dharmesh37nadola@gmail.com>
2023-10-10 00:07:57 +0530

    final working java lambda

---
+++ hello-world-lambda/src/main/java/com/example/HelloWorldLambda.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class HelloWorldLambda {
+    public String handleRequest(Object input) {
+        return "Hello, World!";
+    }
+}
--- src/main/java/com/myorg/CdkWorkshopStack.java
+++ src/main/java/com/myorg/CdkWorkshopStack.java
@@ -2,45 +2,33 @@ package com.myorg;
 
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.lambda.Code;
-import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.apigateway.*;
+import software.amazon.awscdk.services.lambda.*;
 import software.amazon.awscdk.services.lambda.Runtime;
-import software.amazon.awscdk.services.apigateway.LambdaRestApi;
 
 public class CdkWorkshopStack extends Stack {
-    public CdkWorkshopStack(final Construct parent, final String id) {
-        this(parent, id, null);
-    }
+    public CdkWorkshopStack(final Construct scope, final String id) {
+        super(scope, id);
 
-    public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
-        super(parent, id, props);
+        Function helloWorldLambda = Function.Builder.create(this, "HelloWorldLambda")
+                .runtime(Runtime.JAVA_11)
+                .handler("com.example.HelloWorldLambda::handleRequest")
+                .code(Code.fromAsset("../cdk-workshop/hello-world-lambda/target/hello-world-lambda-1.0-SNAPSHOT.jar"))
+                .build();
 
-        //  // Defines a new lambda resource
-        //  final Function hello = Function.Builder.create(this, "HelloHandler")
-        // //  .runtime(Runtime.NODEJS_18_X)    // execution environment
-        //  .runtime(Runtime.JAVA_11)    // execution environment
-        //  .code(Code.fromAsset("HelloWorldFunction"))  // code loaded from the "lambda" directory
-        //  .handler("App.handler")        // file is "hello", function is "handler"
-        //  .build();
+        // Create API Gateway
+        RestApi api = RestApi.Builder.create(this, "HelloWorldAPI")
+                .restApiName("HelloWorldAPI")
+                .build();
 
-        //  // Defines an API Gateway REST API resource backed by our "hello" function
         // LambdaRestApi.Builder.create(this, "Endpoint")
-        // .handler(hello)
-        // .build();
+        //         .handler(helloWorldLambda)
+        //         .build();
 
-         // Define Lambda function
-         Function lambdaFunction = Function.Builder.create(this, "SampleLambdaFunction")
-         .runtime(Runtime.JAVA_11)
-         .code(Code.fromAsset("HelloWorldFunction"))
-         .handler("com.example.YourHandler::handleRequest")
-         .build();
+        // Define API Gateway method and integrate with Lambda function
+        LambdaIntegration integration = LambdaIntegration.Builder.create(helloWorldLambda)
+                .build();
 
- // Create API Gateway and associate with Lambda function
-        LambdaRestApi.Builder.create(this, "SampleApi")
-         .handler(lambdaFunction)
-         .build();
-
-        
+        api.getRoot().addMethod("GET", integration);
     }
 }
